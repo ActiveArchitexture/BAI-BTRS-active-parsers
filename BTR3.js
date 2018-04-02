f7acd157767d8c544e50779c8206210081ac46b9
Ross Kendle <ross.kendle@gmail.com>
2018-04-02 14:27:47 +1000

    Added file trailer. explicit eol not required because BRTSfile is a Syntactic rule

--- BTR3.js
+++ BTR3.js
@@ -29,5 +29,27 @@ assert(g.match('2360', 'fileCreationTime').failed(), 'mm 60 not in range 00..59'
 assert(g.match('01,122099999,123456789,150623,0200,1,,,3/', 'FileHeader').succeeded(), 'ANSI X9.121–2016 (BTR3) Sample 01 Record Example Using Only Mandatory Fields');
 assert(g.match('01, 122099999, 123456789, 150623, 0200, 1,,,3/', 'FileHeader').succeeded(), 'ANSI X9.121–2016 (BTR3) Sample 01 Record Example WITH space after commas');
 
+// Log traces to console - similar to interactive editor outputs
+// console.log(g.trace("2359", "fileCreationTime").toString());
 
-g.trace("2359", "fileCreationTime");
+// Test grammar for fileControlTotal
+assert(g.match('1215450000', 'fileControlTotal').succeeded(), 'unsigned total');
+assert(g.match('-1215450000', 'fileControlTotal').succeeded(), 'negative total');
+assert(g.match('+1215450000', 'fileControlTotal').succeeded(), 'positive total');
+
+// Test grammar for numberofBanks
+assert(g.match('121', 'numberofBanks').succeeded(), 'unsigned numberofBanks');
+assert(g.match('+121', 'numberofBanks').succeeded(), 'positive numberofBanks');
+assert(g.match('-121', 'numberofBanks').failed(), 'negative numberofBanks');
+
+// Test grammar for FileTrailer
+assert(g.match('99,1215450000,4,136/', 'FileTrailer').succeeded(), 'ANSI X9.121–2016 (BTR3) Sample 99 Record');
+assert(g.match('99,0,0,2/', 'FileTrailer').succeeded(), 'ANSI X9.121–2016 (BTR3) from 5.1.1 Empty File 99 Record');
+
+// Test grammar for 5.1.1 Empty File - with CRLF
+var emptyfile = '01,123456789,NAMENAME,150716,2100,11,,,3/' + '\r\n' + '99,0,0,2/'
+assert(g.match(emptyfile, 'BTRSfile').succeeded(), 'ANSI X9.121–2016 (BTR3) 5.1.1 Empty File');
+
+// Test grammar for 5.1.1 Empty File - with CR 
+var emptyfile = '01,123456789,NAMENAME,150716,2100,11,,,3/' + '\r' + '99,0,0,2/'
+assert(g.match(emptyfile, 'BTRSfile').succeeded(), 'ANSI X9.121–2016 (BTR3) 5.1.1 Empty File');
