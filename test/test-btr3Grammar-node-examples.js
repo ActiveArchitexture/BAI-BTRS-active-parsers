bad7b477b364ce6ac16e997a6c1a7f23e06474b6
Ross Kendle <ross.kendle@gmail.com>
2018-04-15 16:58:35 +1000

    Fixed regression with currencyCodeNull in BankHeader.

--- test/test-btr3Grammar-node-examples.js
+++ test/test-btr3Grammar-node-examples.js
@@ -92,6 +92,7 @@ test('grammar for valid BTRSfile succeeds for ANSI X9.121–2016 (BTR3) 5.1.1 Em
 
 test.todo("3.3 ACCOUNT HEADER – Record 03 customerAccountNumber Must not contain comma , or slash / delimiters.");
 test.todo("3.3.1 Sample 03 Record Example Splitting a Summary Code (400) Across 2 Lines");
+test.todo("Extract statusTypeCode and summaryTypeCode and descriptions to separate grammars");
 
 
 function btr3Parse(source, startNode) {
